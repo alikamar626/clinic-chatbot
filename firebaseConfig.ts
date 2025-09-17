653759364a39299f91ebc554c5a643755b199002
Ali kamaredine <149944617+alikamar626@users.noreply.github.com>
2025-09-17 22:55:39 +0300

    firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,16 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjUPXoCmW6DbjB2697j6CC8wcBpctP-NU",
-  authDomain: "isd-2025.firebaseapp.com",
-  projectId: "isd-2025",
-  storageBucket: "isd-2025.firebasestorage.app",
-  messagingSenderId: "559081729524",
-  appId: "1:559081729524:web:3e1cab29454ce9093c5da1",
+  apiKey: "....",
+  authDomain: "....",
+  projectId: "...",
+  storageBucket: "....",
+  messagingSenderId: "...",
+  appId: ".....",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app); // Firestore database
+
 
 export { auth, db };
